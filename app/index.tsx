eb890921148476e7a239443191ce4fbab34ccec1
A-dith-ya <awedithya@gmail.com>
2024-07-15 15:44:18 -0700

    Add Contentful to fetch questions data

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,32 @@
 import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import client from "../services/contentfulService";
+
+interface Question {
+  question: string;
+  options: {
+    option: string;
+    subOptions: string[];
+    filter?: string;
+  }[];
+}
 
 export default function Index() {
+  const [questionsData, setQuestionsData] = useState<Question[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const entry = await client.getEntry("3lRhUvpZ7NaUuPyVtarAZ");
+        setQuestionsData(entry.fields.questions.questions);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View
       style={{
