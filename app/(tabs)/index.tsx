c26823b5fd970b1600771d5af0e58f1fe74e875d
A-dith-ya <awedithya@gmail.com>
2024-08-06 12:48:29 -0700

    Start rendering webview after user selects an option

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -84,24 +84,26 @@ export default function QuestionScreen() {
     filter: string,
     index: number,
     customWebViewUri?: string
-  ) =>
+  ) => (
     // Render webview only after the user navigates completely through the question screen
-    questionNumber % 1 === 0.5 ? (
-      <WebView
-        ref={(ref) => (webviewRefs.current[index] = ref!)}
-        style={styles.webview}
-        source={{
-          // Use the suboption webview uri otherwise use the default webview uri
-          uri: customWebViewUri || webViewUri,
-        }}
-        // Inject the javascript to scroll to the element in the webview
-        onLoad={() =>
-          webviewRefs.current[index]?.injectJavaScript(
-            SCROLL_TO_SYMPTOM(filter)
-          )
-        }
-      />
-    ) : null;
+    <WebView
+      key={`webview_${index}`}
+      ref={(ref) => (webviewRefs.current[index] = ref!)}
+      style={[
+        // Start rendering webview after user selects an option
+        styles.webview,
+        !(questionNumber % 1 === 0.5) && styles.hideWebView,
+      ]}
+      source={{
+        // Use the suboption webview uri otherwise use the default webview uri
+        uri: customWebViewUri || webViewUri,
+      }}
+      // Inject the javascript to scroll to the element in the webview
+      onLoad={() =>
+        webviewRefs.current[index]?.injectJavaScript(SCROLL_TO_SYMPTOM(filter))
+      }
+    />
+  );
 
   const renderRadioButton = (option: string) => (
     <RadioButton
@@ -302,6 +304,11 @@ const styles = StyleSheet.create({
     height: height * 0.45,
     marginVertical: height * 0.01,
     marginHorizontal: width * 0.05,
+  },
+  hideWebView: {
+    position: "absolute",
+    left: -1000,
+    right: -1000,
   },
   buttonContainer: {
     width: width * 0.9,
