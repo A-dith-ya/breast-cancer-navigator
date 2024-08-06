0127c3fd6d2dae3ec4e360bc076a8a3636b0a214
A-dith-ya <awedithya@gmail.com>
2024-08-06 08:05:04 -0700

    Fix scroll behavior to return to element initial position on web view

--- constants/InjectedJavascript.ts
+++ constants/InjectedJavascript.ts
@@ -1,10 +1,22 @@
 const scrollToSymptom = (item: string) => `
-let elements = Array.from(document.querySelectorAll('span.ou-accordion-label'));
-let element = elements.find(element => element.textContent.toLowerCase().includes('${item}'.toLowerCase()));
-if (element) {
-  element.scrollIntoView();
-  element.click();
-}
+  let elements = Array.from(document.querySelectorAll('span.ou-accordion-label'));
+  let element = elements.find(element => element.textContent.toLowerCase().includes('${item}'.toLowerCase()));
+  if (element) {
+    element.scrollIntoView();
+    element.click();
+  }
+
+  const scrollHandler = () => {
+    element.scrollIntoView();
+  };
+
+  const touchHandler = () => {
+    window.removeEventListener('scroll', scrollHandler);
+    window.removeEventListener('touchstart', touchHandler);
+  };
+  
+  window.addEventListener('scroll', scrollHandler);
+  window.addEventListener('touchstart', touchHandler);
 true; // note: this is required, or you'll sometimes get silent failures
 `;
 
