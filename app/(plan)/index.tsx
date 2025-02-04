70ee4dfaee656acd154653925be18ad8a807cc37
A-dith-ya <awedithya@gmail.com>
2025-02-03 22:18:16 -0800

    Add navigation to meal, fitness, and wellbeing screens in DashboardScreen

--- app/(plan)/index.tsx
+++ app/(plan)/index.tsx
@@ -1,27 +1,33 @@
 import { StyleSheet, Dimensions } from "react-native";
 import React from "react";
+import { useRouter } from "expo-router";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedButton } from "@/components/ThemedButton";
 
 const { width, height } = Dimensions.get("window");
 
 export default function DashboardScreen() {
+  const router = useRouter();
+
   return (
     <ThemedView style={styles.container}>
       <ThemedButton
         text="Nutrition & Meal Plan"
         style={styles.button}
         type="buttonTitle"
+        onPress={() => router.push("/(plan)/meal-plan")}
       />
       <ThemedButton
         text="Exercise & Mobility"
         style={styles.button}
         type="buttonTitle"
+        onPress={() => router.push("/(plan)/fitness-plan")}
       />
       <ThemedButton
         text="Mind & Wellness"
         style={styles.button}
         type="buttonTitle"
+        onPress={() => router.push("/(plan)/wellbeing-plan")}
       />
     </ThemedView>
   );
